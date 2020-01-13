1ebf5090df41c4fbc9ac3806eb8fa9c192a3a229
KenMwaura1 <kemwaura@gmail.com>
2020-01-13 17:57:20 +0300

    updates to docker

---
+++ src/tests/test_main.py
@@ -0,0 +1,10 @@
+from starlette.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+def test_ping():
+    response = client.get("/ping")
+    assert response.status_code == 200
+    assert response.json() == {"ping":"pong"}
