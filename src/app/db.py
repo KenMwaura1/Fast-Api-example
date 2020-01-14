e151ef4a4ce50f983dd48d53bd5d763208c1d950
KenMwaura1 <kemwaura@gmail.com>
2020-01-14 19:22:24 +0300

    Added database functionality updated the .env file

--- src/app/db.py
+++ src/app/db.py
@@ -4,13 +4,20 @@ from sqlalchemy import (Column, DateTime, Integer, String, Table, create_engine,
 from sqlalchemy.sql import func
 from databases import Database
 
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+# Database url if none is passed the default one is used
+DATABASE_URL = os.getenv("DATABASE_URL","postgresql://hello_fastapi:hello_fastapi@localhost/hello_fastapi_dev")
 
 #SQLAlchemy
 engine = create_engine(DATABASE_URL)
-metadata - MetaData()
-
+metadata = MetaData()
+notes = Table(
+    "notes",
+    metadata,
+    Column("id", Integer,primary_key=True),
+    Column("title",String(50)),
+    Column("description",String(50)),
+    Column("created_date",DateTime, default=func.now(),nullable=False)
+)
 # Databases query builder
 
 database = Database(DATABASE_URL)
