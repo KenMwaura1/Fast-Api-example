5ee61a15b9f47999cb91707cfaf176a7abcb7c15
KenMwaura1 <kemwaura@gmail.com>
2020-01-14 20:49:34 +0300

    post for creating new notes

---
+++ src/app/api/crud.py
@@ -0,0 +1,7 @@
+rom app.api.models import NoteSchema
+from app.db import notes, database
+
+
+async def post(payload: NoteSchema):
+    query = notes.insert().values(title=payload.title, description=payload.description)
+    return await database.execute(query=query)
