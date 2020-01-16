841e0c24c8181960eea5b68f995c3cd8e848c330
KenMwaura1 <kemwaura@gmail.com>
2020-01-16 12:45:42 +0300

    Updated tests to reflect additional validation

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -22,6 +22,8 @@ def test_create_note(test_app, monkeypatch):
 def test_create_note_invalid_json(test_app):
     response = test_app.post("/notes/", data=json.dumps({"title": "something"}))
     assert response.status_code == 422
+    response = test_app.post("/notes/", data=json.dumps({"title": "1", "description": "2"}))
+    assert response.status_code == 422
 
 # These tests should fail 
 def test_read_note(test_app, monkeypatch):
@@ -101,6 +103,9 @@ def test_update_note(test_app, monkeypatch):
         [1, {}, 422],
         [1, {"description": "bar"}, 422],
         [999, {"title": "foo", "description": "bar"}, 404],
+        [1, {"title": "1", "description": "bar"}, 422],
+        [1, {"title": "foo", "description": "1"}, 422],
+        [0, {"title": "foo", "description": "bar"}, 422],
     ],
 )
 def test_update_note_invalid(test_app, monkeypatch, id, payload, status_code):
@@ -140,3 +145,6 @@ def test_remove_note_incorrect_id(test_app, monkeypatch):
     response = test_app.delete("/notes/999/")
     assert response.status_code == 404
     assert response.json()["detail"] == "Note not found"
+
+    response = test_app.delete("/notes/0/")
+    assert response.status_code == 422
