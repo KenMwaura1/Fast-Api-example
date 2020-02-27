b8af4537c09b80e7ab8e45b231240e387e2cc760
KenMwaura1 <kemwaura@gmail.com>
2020-02-27 20:49:33 +0300

    Remove ignored files

--- dist/static/js/2.4a352d5b35197462404c.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],{c27y:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l={render:function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("About Page")])},staticRenderFns:[]},r=n("VU/8")(null,l,!1,null,null,null);t.default=r.exports}});
-//# sourceMappingURL=2.4a352d5b35197462404c.js.map
