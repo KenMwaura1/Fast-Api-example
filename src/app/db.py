f7670a7378813eccc8aa6c424323fa06f99ca734
KenMwaura1 <kemwaura@gmail.com>
2023-01-28 19:23:30 +0300

    fixed column

--- src/app/db.py
+++ src/app/db.py
@@ -5,6 +5,7 @@ from sqlalchemy.sql import func
 from dotenv import load_dotenv
 from databases import Database
 from datetime import datetime as dt
+from pytz import timezone as tz
 
 load_dotenv()
 # Database url if none is passed the default one is used
@@ -20,7 +21,7 @@ notes = Table(
     Column("title", String(50)),
     Column("description", String(50)),
     Column("completed",String(8), default="False"),
-    Column("created_date", DateTime, default=func.now(), nullable=False)
+    Column("created_date", String(50), default=dt.now(tz("Africa/Nairobi")).strftime("%Y-%m-%d %H:%M"))
 )
 # Databases query builder
 
