63ff0e70bbb9407b32a91e74bdaf9a7db7c4fe46
KenMwaura1 <kemwaura@gmail.com>
2023-01-28 19:24:20 +0300

    updated the date column

--- src/app/api/models.py
+++ src/app/api/models.py
@@ -1,16 +1,14 @@
-import datetime
-from pydantic import BaseModel, Field
 
+from pydantic import BaseModel, Field, NonNegativeInt
+from datetime import datetime as dt
 
 class NoteSchema(BaseModel):
     title: str = Field(..., min_length=3, max_length=50) #additional validation for the inputs 
     description: str = Field(...,min_length=3, max_length=50)
     completed: str = Field(...,min_length=3, max_length=8) 
-    created_date: datetime.datetime = Field(default_factory=datetime.datetime.now)
 
 
 class NoteDB(NoteSchema):
     id: int 
-    created_date: datetime.datetime
-
+    created_date: str
 
--- src/app/api/notes.py
+++ src/app/api/notes.py
@@ -9,7 +9,7 @@ router = APIRouter()
 @router.post("/", response_model=NoteDB, status_code=201)
 async def create_note(payload: NoteSchema):
     note_id = await crud.post(payload)
-    created_date = dt.now()
+    created_date = dt.now().strftime("%Y-%m-%d %H:%M")
 
     response_object = {
         "id": note_id,
