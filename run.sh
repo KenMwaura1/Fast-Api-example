a4a1afb4cd32344c63da157ab57275f4db407e94
KenMwaura1 <kemwaura@gmail.com>
2023-02-03 01:51:13 +0300

    removed signing the docker image

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export APP_MODULE=${APP_MODULE-src.main:app}
+export HOST=${HOST:-0.0.0.0}
+export PORT=${PORT:-8001}
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# The program is run with the following command:
+exec uvicorn --reload --host "$HOST" --port "$PORT" "$APP_MODULE"
+
+# uvicorn app.main:app --reload --workers 1 --host 0.0.0.0 --port 8003
