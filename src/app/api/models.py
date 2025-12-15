b1bf2010a4c2a9db249173e9210cbdf1a24dd421
KenMwaura1 <kemwaura@gmail.com>
2025-12-15 16:03:12 +0300

    refactor: update NoteSchema to use proper data types for completed and created_date fields

--- src/app/api/models.py
+++ src/app/api/models.py
@@ -1,14 +1,13 @@
 from pydantic import BaseModel, Field
-from datetime import datetime as dt
-from pytz import timezone as tz
+from datetime import datetime
 
 
 class NoteSchema(BaseModel):
-    title: str = Field(..., min_length=3, max_length=50)  # additional validation for the inputs
+    title: str = Field(..., min_length=3, max_length=50)
     description: str = Field(..., min_length=3, max_length=50)
-    completed: str = "False"
-    created_date: str = dt.now(tz("Africa/Nairobi")).strftime("%Y-%m-%d %H:%M")
+    completed: bool = False
 
 
 class NoteDB(NoteSchema):
     id: int
+    created_date: datetime
